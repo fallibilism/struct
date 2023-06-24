050e142f80d5ecbaae1164312de2bba193e94865
amar-jay <64834413+amar-jay@users.noreply.github.com>
2023-06-24 19:18:18 +0000

    error: postgres cannot run in codespace

--- pkg/config/postgres.go
+++ pkg/config/postgres.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,22 +11,14 @@ const DBConnectionError = "failed to connect to db"
 
 func NewDbConnection(c *PostgresConfig) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s", c.Host, c.Username, c.Password, c.DBName, c.Port, c.SslMode, c.TimeZone)
-	ticker := time.NewTicker(5 * time.Second)
 
 	var db *gorm.DB
 	var err error
-	for range ticker.C {
-		if db != nil {
-			break
-		}
 
-		fmt.Printf("running db connection again")
-
-		db, err = gorm.Open(postgres.New(postgres.Config{
-			DSN: dsn,
-			// PreferSimpleProtocol: true, // disables implicit prepared statement usage
-		}), &gorm.Config{})
-	}
+	db, err = gorm.Open(postgres.New(postgres.Config{
+		DSN: dsn,
+		// PreferSimpleProtocol: true, // disables implicit prepared statement usage
+	}), &gorm.Config{})
 
 	if err != nil {
 		return nil, fmt.Errorf("%s: %v", DBConnectionError, err)
